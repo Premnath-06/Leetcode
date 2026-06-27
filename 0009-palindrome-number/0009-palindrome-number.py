2acc16a5dc20e9e701ca6ad4e8086861ca939a13
Premnath_Rajule <mosqiiplayz@gmail.com>
2026-06-27 21:12:05 +0530

    Time: 10 ms (59.52%), Space: 12.3 MB (89.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        reverted_num = 0
+        while x > reverted_num:
+            reverted_num = reverted_num * 10 + x % 10
+            x //= 10
+        return x == reverted_num or x == reverted_num // 10
