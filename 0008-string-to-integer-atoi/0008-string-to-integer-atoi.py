dfe91d391b3cea649c165a465ce0161e2fd7a14f
Premnath_Rajule <mosqiiplayz@gmail.com>
2026-06-27 21:09:53 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.47%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -4,16 +4,17 @@ class Solution(object):
         if not s:
             return 0
         sign = 1
-        i = 0
         if s[0] == '-':
             sign = -1
-            i += 1
+            s = s[1:]
         elif s[0] == '+':
-            i += 1
+            s = s[1:]
         res = 0
-        while i < len(s) and s[i].isdigit():
-            res = res * 10 + int(s[i])
-            i += 1
+        for char in s:
+            if '0' <= char <= '9':
+                res = res * 10 + (ord(char) - 48)
+            else:
+                break
         res *= sign
         if res < -2147483648:
             return -2147483648
