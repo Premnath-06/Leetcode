464952b6f59786425ad401eaf5d30d368391a384
Premnath Rajule <mosqiiplayz@gmail.com>
2026-06-30 13:16:35 +0530

    Time: 0 ms (100%), Space: 13 MB (67.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,3 +6,8 @@ class Solution(object):
             if complement in seen:
                 return [seen[complement], i]
             seen[num] = i
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
