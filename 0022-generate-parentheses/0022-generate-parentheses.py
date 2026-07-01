d6acacbf279cd609a8f8d1de9758ee9ab3b19b94
Premnath Rajule <mosqiiplayz@gmail.com>
2026-07-01 23:32:28 +0530

    Time: 0 ms (100%), Space: 12.8 MB (19.29%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        res = []
+        def backtrack(current_string, open_count, close_count):
+            if len(current_string) == 2 * n:
+                res.append(current_string)
+                return
+            if open_count < n:
+                backtrack(current_string + '(', open_count + 1, close_count)
+            if close_count < open_count:
+                backtrack(current_string + ')', open_count, close_count + 1)
+        backtrack("", 0, 0)
+        return res
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
